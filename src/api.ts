06126325113523efa0c3df96fc05a2c21c497c44
ndmanh2 <ndmanh2@cmcglobal.vn>
2024-06-17 21:34:16 +0700

    chore: update logic

--- src/api.ts
+++ src/api.ts
@@ -9,9 +9,12 @@ app.use(express.json());
 app.use(express.raw({ type: 'application/vnd.custom-type' }));
 app.use(express.text({ type: 'text/html' }));
 
+
+const mockDatabase = [];
+
 // Healthcheck endpoint
 app.get('/', (req, res) => {
-  res.status(200).send({ status: 'ok' });
+  res.status(200).send({ status: 'ok1' });
 });
 
 const api = express.Router();
@@ -19,6 +22,19 @@ const api = express.Router();
 api.get('/hello', (req, res) => {
   res.status(200).send({ message: 'hello world' });
 });
+
+api.post('/nangluong', (req, res) => {
+
+  const data = req.body;
+
+  mockDatabase.push(data);
+
+  res.status(200).send({ message: 'ok' });
+})
+
+api.get('/nangluong', (req, res) => {
+  res.status(200).send({ data: mockDatabase });
+})
 
 // Version the api
 app.use('/api/v1', api);
